beaa35c07877aa90c558b79b875a5a5599868bd1
bammss <bambangsugiarto200316@gmail.com>
2020-02-05 22:41:38 +0700

    fix url

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,40 +1,41 @@
 <?php
-class Home extends CI_Controller {
+class Home extends CI_Controller
+{
 
 	public function index()
 	{
-        $this->load->model("model_data");
+		$this->load->model("model_data");
 		$data['list_karyawan'] = $this->model_data->read();
 
-		$this->load->view("data_karyawan",$data);
-    }
-    
-    public function add()
-    {
-		$this->load->model("model_data");	
-        $data['tipe'] = "Add";
-		
-		if(isset($_POST['submit']))
-		{
+		$this->load->view("data_karyawan", $data);
+	}
+
+	public function add()
+	{
+		$this->load->model("model_data");
+		$data['tipe'] = "Add";
+
+		if (isset($_POST['submit'])) {
 			//proses simpan dilakukan
 			$this->model_data->simpan($_POST);
 			redirect("home");
 		}
-			
-		$this->load->view("form_karyawan",$data);
+
+		$this->load->view("form_karyawan", $data);
 	}
 
-	public function edit($id){
+	public function edit($id)
+	{
 		$this->load->model("model_data");
 
 		$data['tipe'] = "Update";
-		if(isset($_POST['submit'])){
+		if (isset($_POST['submit'])) {
 			$this->model_data->update($_POST, $id);
 			redirect("home");
 		}
 
 		$data['default'] = $this->model_data->get_default($id);
-		$this->load->view("form_karyawan",$data);
+		$this->load->view("form_karyawan", $data);
 	}
 
 	public function delete($id)
@@ -43,6 +44,4 @@ class Home extends CI_Controller {
 		$this->model_data->hapus($id);
 		redirect("home");
 	}
-
 }
-
--- application/views/data_karyawan.php
+++ application/views/data_karyawan.php
@@ -1,43 +1,45 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>CRUD_CI</title>
-  </head>
-  <body>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+	<title>CRUD_CI</title>
+</head>
+
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+		<div class="container">
+			<a class="navbar-brand" href="<?= base_url('home'); ?>">CRUD_CI</a>
+			<a href="<?= base_url('home/add'); ?>" class="btn btn-primary">Add Data</a>
+		</div>
+	</nav>
+
 	<div class="container">
-		<a class="navbar-brand" href="">CRUD_CI</a>
-		<a href="home/add" class="btn btn-primary">Add Data</a>
-	</div>
-  </nav>
-
-  <div class="container">
-  	<h5 class="mb-1">CRUD Aplication</h5>
-    <small>CRUD / CodeIgniter</small>
-	<table class="table mt-3">
-		<thead>
-			<tr>
-				<th>No</th>
-				<th>Nama Karyawan</th>
-				<th>Email</th>
-				<th>Alamat</th>
-				<th></th>
-			</tr>
-		</thead>
-		<tbody>
-			<!-- ISI DATA AKAN MUNCUL DISINI -->
-			<?php
-			$no = 1; //untuk menampilkan no
-			foreach($list_karyawan as $row){
-				echo "
+		<h5 class="mb-1">CRUD Aplication</h5>
+		<small>CRUD / CodeIgniter</small>
+		<table class="table mt-3">
+			<thead>
+				<tr>
+					<th>No</th>
+					<th>Nama Karyawan</th>
+					<th>Email</th>
+					<th>Alamat</th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				<!-- ISI DATA AKAN MUNCUL DISINI -->
+				<?php
+				$no = 1; //untuk menampilkan no
+				foreach ($list_karyawan as $row) {
+					echo "
 				<tr>
 					<td>$no</td>
 					<td>$row[nama]</td>
@@ -50,18 +52,19 @@
 					</td>
 				</tr>
 				";
-				$no++;
-			}
-			?>
-		</tbody>
-	</table>
-</div>
-    
+					$no++;
+				}
+				?>
+			</tbody>
+		</table>
+	</div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </body>
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
 </html>
